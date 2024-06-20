e06f9500a294ac3bcc000b64d3da7d0f629218e2
Sayan650 <sayanp650@gmail.com>
2024-06-21 01:21:09 +0530

    members list fix

--- data/index.ts
+++ data/index.ts
@@ -11,6 +11,84 @@ export const secondYear = [
     img: "/Images/2nd/benny.jpeg",
     social: "social link",
   },
+  {
+    id: 3,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 4,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 5,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 6,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 7,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 7,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 8,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 9,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 10,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 11,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 12,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 13,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
+  {
+    id: 14,
+    name: "Benny",
+    img: "/Images/2nd/benny.jpeg",
+    social: "social link",
+  },
 ];
 
 export const thirdYear = [
